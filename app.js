2413bc3075be61c78d7bc5a3811ea5c5651c56be
bharat_patil <patilbharat09@gmail.com>
2020-07-14 15:45:12 +0530

    Added graphql typeto get events and create event

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql").graphqlHTTP;
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-  res.send("Hello World")
-})
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
 
-app.listen(4201)
+    type RootMutation {
+      createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery,
+      mutation: RootMutation
+    }
+  `),
+    rootValue: {
+      events: () => {
+        return ["test", "test 12", "test 2323", "test 243 43"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
+
+app.listen(4201);
